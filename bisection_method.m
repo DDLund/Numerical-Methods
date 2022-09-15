8c1cfbf3134945b75f1cbe9f41fd7c737345b97a
Daniel D Lundstrom <76541425+DDLund@users.noreply.github.com>
2022-09-15 14:16:50 -0700

    Add files via upload

---
+++ bisection_method.m
@@ -0,0 +1,19 @@
+
+f = @(x) x.^-1 - 2^x;       % test function to run bisection method on
+a=0; b=1;                   % two endponts for bisection method
+p = (a + b)/2;              % first midpoint
+err = abs(f(p));             % current error
+
+% main loop, bisect until error is small
+while err > 1e-7            
+    if f(a)*f(p)<0
+        b = p;
+    else
+        a = p;
+    end
+    p = (a + b)/2;
+    err = abs(f(p));
+end
+
+p                           % Approximate root
+err                         % Error
