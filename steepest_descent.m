baa09456b99775537ee4b56b30dd20532a563013
Daniel D Lundstrom <76541425+DDLund@users.noreply.github.com>
2022-09-16 09:15:26 -0700

    Update and rename SteepestDescent.m to steepest_descent.m

---
+++ steepest_descent.m
@@ -0,0 +1,20 @@
+% Steepest descent method of solving Ax=b
+
+A=[4,-1,-1,0;-1,4,0,-1;-1,0,4,-1;0,-1,-1,4];
+b=[-4;0;4;-4];
+x0=[0;0;0;0];
+[x,r] = stepDesc(A,b,x0,10);
+x
+r
+e
+
+
+function [x,r,e] = stepDesc(A,b,x,n)
+for i=1:n
+    v=b-A*x;
+    t=v'*v/(v'*A*v);
+    x=x+t*v;
+end
+r=A*x-b;
+e=x-A^-1*b;
+end
